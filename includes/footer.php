2f0a9ea84a5959bae4f0ac98e09e151720ba179e
FilipHavlik223 <filiphavlik223@seznam.cz>
2025-11-27 22:20:55 +0100

    header and footer

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="bg-light text-center p-4 mt-5">
+    <p>&copy; 2025 Blog by Filip Havlík</p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <strong><a class="navbar-brand" href="index.php">Můj Blog</a></strong>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Domů</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?stranka=kontakt">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
